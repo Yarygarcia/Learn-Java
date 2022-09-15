45d46bd2912dbe70d5436704693a6e0c1815dfa7
yarygarcia <yarygarciaq@hotmail.com>
2022-09-15 17:46:54 -0500

    Clase Principal

--- gestion aeroportuaria/src/AeropuertoPublico.java
+++ gestion aeroportuaria/src/AeropuertoPublico.java
@@ -2,10 +2,15 @@ public class AeropuertoPublico extends Aeropuerto{
 
     private double dineroNacion;
 
-    public AeropuertoPublico(String nombre, String ciudad, String pais){
+    public AeropuertoPublico(String nombre, String ciudad, String pais) {
         super(nombre, ciudad, pais);
     }
-    public AeropuertoPublico(String nombre, String ciudad, String pais, double dineroNacion) {
+    public AeropuertoPublico(String nombre, String ciudad, String pais, Company[] listCompanies, double dineroNacion) {
+        super(nombre, ciudad, pais, listCompanies);
+        this.dineroNacion = dineroNacion;
+    }
+
+    public  AeropuertoPublico(String nombre, String ciudad, String pais, double dineroNacion){
         super(nombre, ciudad, pais);
         this.dineroNacion = dineroNacion;
     }
