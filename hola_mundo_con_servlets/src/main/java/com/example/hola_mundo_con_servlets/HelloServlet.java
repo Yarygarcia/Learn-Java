e983c7909d6f612dbdaa8b69477cbab7b4b6e10b
yarygarcia <yarygarciaq@hotmail.com>
2023-01-06 21:49:02 -0500

    servlets

---
+++ hola_mundo_con_servlets/src/main/java/com/example/hola_mundo_con_servlets/HelloServlet.java
@@ -0,0 +1,28 @@
+package com.example.hola_mundo_con_servlets;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World! desde la clase con servlets";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("<h1> Iniciando en servlet</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
